316205dfa30d0d1d8800187e40d46aae19b758dd
SaltodElGatod <salwangaming1@gmail.com>
2024-03-21 08:10:33 +0900

    first commit

--- TugasKonversiNilai.py
+++ TugasKonversiNilai.py
@@ -1,20 +1,20 @@
 masukkan_bilangan = int(input("Masukkan Angka Anda Yang Diinginkan: "))
 masukkan_pilihan_user = int(input("Silahkan Pilih 1, 2, 3, atau 4: "))
 
-bil_oktal = oct(masukkan_bilangan)[2:]
-bil_hex = hex(masukkan_bilangan)[2:]
-bil_des = masukkan_bilangan
 bil_bin = bin(masukkan_bilangan)[2:]
+bil_oktal = oct(masukkan_bilangan)[2:]
+bil_des = masukkan_bilangan
+bil_hex = hex(masukkan_bilangan)[2:]
 
 def pilih_operasi(pilihan):
     if pilihan == 1:
-        print("Bilangan Oktal Dari", masukkan_bilangan, "Adalah", bil_oktal)
+        print("Bilangan Biner Dari", masukkan_bilangan, "Adalah", bil_bin)
     elif pilihan == 2:
-        print("Bilangan Hexadesimal Dari", masukkan_bilangan, "Adalah", bil_hex)
+        print("Bilangan Oktal Dari", masukkan_bilangan, "Adalah", bil_oktal)
     elif pilihan == 3:
         print("Masukkan Bilangan Desimal Dari", masukkan_bilangan, "Adalah", bil_des)
     elif pilihan == 4:
-        print("Bilangan Biner Dari", masukkan_bilangan, "Adalah", bil_bin)
+        print("Bilangan Hexadesimal Dari", masukkan_bilangan, "Adalah", bil_hex)
     else:
         print("Pilihan Tidak Sesuai")
 
